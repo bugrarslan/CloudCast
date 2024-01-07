ff6889904d5e7c71b2d1c881912e330d06f587c9
bugrarslan <bugrarsln7@gmail.com>
2024-01-07 15:34:22 +0300

    Onboarding Screen ayarlandı

--- src/screens/OnboardScreen.js
+++ src/screens/OnboardScreen.js
@@ -1,54 +1,52 @@
 import React, { useEffect, useState } from "react";
-import { Dimensions, Text, TouchableOpacity, View} from "react-native";
+import { Dimensions, ImageBackground, Text, TouchableOpacity, View} from "react-native";
 import FastImage from "react-native-fast-image";
 import { check, PERMISSIONS, RESULTS, request} from "react-native-permissions";
+import { PermissionsAndroid } from 'react-native';
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
 const OnboardScreen = ({navigation}) => {
 
-  const requestPermission = () => {
-    request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION).then((response) => {
-      console.log(response)
-    })
-  }
+  const handlePermissionCheck = async () => {
+    try {
+      // İzin kontrolü burada gerçekleştirilir
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        {
+          title: 'Uygulama İzni',
+          message: 'Uygulamayı kullanabilmek için konum izni gerekiyor.',
+          buttonNeutral: 'Daha sonra sor',
+          buttonNegative: 'Reddet',
+          buttonPositive: 'İzin Ver',
+        },
+      );
+      
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        // İzin verildiyse ana sayfaya yönlendirme
+        navigation.navigate('Home');
+      } else {
+        console.log('İzin reddedildi');
+        // İzin reddedildiyse isteğinize göre işlemleri yapabilirsiniz
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
 
-  const checkPermission = () => {
-    check(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION)
-      .then((result) => {
-        switch (result) {
-          case RESULTS.DENIED:
-            requestPermission();
-            break;
-          case RESULTS.GRANTED:
-            navigation.navigate("Home");
-            break;
-          case RESULTS.BLOCKED:
-            requestPermission();
-            break;
-        }
-      })
-      .catch((error) => {
-        // …
-      });
-  }
-
-  const handleNavigation = () => {
-    checkPermission()
-  }
+  useEffect(() => {
+    // Component yüklendiğinde izin kontrolü yapılır
+    handlePermissionCheck();
+  }, []);
 
   return(
-    <View style={{flex:1, alignItems:"center", justifyContent:"center"}}>
-      <FastImage source={require("../assets/images/bg.png")} resizeMode={FastImage.resizeMode.cover} style={{width:windowWidth, height:windowHeight, position:"absolute", top:0}}/>
-      <View style={{flexDirection:"row", alignItems:"center", justifyContent:"space-around",width:windowWidth}}>
-        <Text style={{textAlign:"center", color:"white", fontSize:15, fontWeight:"bold"}}>Welcome to the CloudCast</Text>
-        <TouchableOpacity
-          onPress={()=> {handleNavigation()}}
-          style={{ width:windowWidth/5, height:windowHeight/9, justifyContent:"center", alignItems:"center", borderRadius:40, borderWidth:2, borderColor:"gray"}}>
-          <Text style={{ textAlign:"center", fontSize:20}}>Go</Text>
-        </TouchableOpacity>
+    <View style={{flex:1}}>
+      <ImageBackground source={require("../assets/images/bg.png")} blurRadius={70} style={{ width: windowWidth, height: windowHeight, alignItems:"center", justifyContent:"center" }}>
+        <View style={{flexDirection:"row", alignItems:"center", justifyContent:"space-around",width:windowWidth}}>
+        <Text style={{textAlign:"center", color:"white", fontSize:20, fontWeight:"bold"}}>Welcome to the CloudCast</Text>
       </View>
+      </ImageBackground>
     </View>
   )
 }
